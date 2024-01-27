cc376f293dbf9038931fd4db8889a337efca6559
Yurii Zinets <yurii.zinets@icloud.com>
2024-01-27 01:11:34 +0100

    try to fix build

--- docsgen.sh
+++ docsgen.sh
@@ -2,4 +2,4 @@
 # Kyoto docs
 
 echo "Generating kyoto-framework/kyoto docs ..."
-python3 -m docsgen -r https://github.com/kyoto-framework/kyoto.git -b v3 -o ./kyoto
+python3 -m docsgen -r https://github.com/kyoto-framework/kyoto.git -o ./kyoto
